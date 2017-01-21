07f939ce84e5e3c4d27d315ffbc84a6bd3a87d73
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-01-21 20:56:18 +0000

    making walls and treasre unwalkable

--- Assets/scripts/player_collision.cs
+++ Assets/scripts/player_collision.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class player_collision : MonoBehaviour {
 
 	int treasure_collected = 0;
-
+	public AudioSource audioSource;
+	public AudioClip pickup_sound;
 	// Use this for initialization
 	void Start () {
-		
+		audioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			
+		
 	}
 
 	void OnTriggerEnter(Collider col) {
 		if (col.gameObject.CompareTag ("Treasure")) {
+			if (audioSource != null) {
+				audioSource.PlayOneShot (pickup_sound);
+			}
 			col.gameObject.SetActive (false);
 			treasure_collected ++;
 		}
