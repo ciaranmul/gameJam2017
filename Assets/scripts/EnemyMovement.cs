500528e61558eabfe44c149c534d32ab63c14cac
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-01-22 02:17:08 +0000

    adding enemy movement towards player

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (player.position);
+	}
+}
