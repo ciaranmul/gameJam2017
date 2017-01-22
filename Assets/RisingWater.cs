702230afa4ab3bd469630d1d0886cbff117e1d0a
CiaranGallagher <ciarangallagher036@gmail.com>
2017-01-22 00:11:41 +0000

    Rising water.

---
+++ Assets/RisingWater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+using Valve.VR;
+
+public class RisingWater : MonoBehaviour
+{
+    private float _min = 0.4f;
+    private float _max = 1.2f;
+    private float _speed = 0.01f;
+    private float _velocity;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    transform.position = new Vector3(transform.position.x, _min);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		transform.position += new Vector3(transform.position.x, _speed * Time.deltaTime);
+	}
+}
